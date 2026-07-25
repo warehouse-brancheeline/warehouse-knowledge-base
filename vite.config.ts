1083f914a5167ce1593697f27ee6a26b1665459c
warehouse-brancheeline <headwarehouse.brancheeline@gmail.com>
2026-07-25 18:31:16 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,11 @@
-import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 import { defineConfig } from 'vite';
 
-export default defineConfig(() => {
-  return {
-    // ✅ WAJIB UNTUK GITHUB PAGES
-    base: '/warehouse-knowledge-base/',
-
-    plugins: [react(), tailwindcss()],
-
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
-
-    server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
-      watch: process.env.DISABLE_HMR === 'true' ? null : {},
-    },
-  };
+export default defineConfig({
+  base: '/warehouse-knowledge-base/',
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
 });
